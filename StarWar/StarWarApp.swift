0ad2105696aed188df3c5deefaade15549632275
Konstantine Tsirgvava <koka.wirgvava@gmail.com>
2024-06-21 16:55:53 +0400

    Initial Commit

---
+++ StarWar/StarWarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarApp.swift
+//  StarWar
+//
+//  Created by Konstantine Tsirgvava on 21.06.24.
+//
+
+import SwiftUI
+
+@main
+struct StarWarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
